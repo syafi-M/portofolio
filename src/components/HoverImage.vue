d2d186f9de3e2f3fb12e33714f400df97b746ce0
syafi-M <syafimq00@gmail.com>
2025-08-12 12:03:02 +0700

    porto test

--- src/components/HoverImage.vue
+++ src/components/HoverImage.vue
@@ -1,29 +1,34 @@
-<!-- components/HoverImage.vue -->
 <template>
   <div
     class="relative w-[140px] h-[140px] rounded-sm overflow-hidden"
     @mouseenter="onEnter"
     @mouseleave="onLeave"
   >
-    <!-- Base image -->
-    <img
-      :src="src"
-      alt="base"
-      class="absolute inset-0 w-full h-full object-cover scale-115 transition-opacity duration-300"
+    <!-- Base image as background -->
+    <div
+      class="absolute inset-0 w-full h-full object-cover scale-115 transition-opacity duration-300 bg-center bg-cover"
+      :style="{ backgroundImage: `url(${src})` }"
       :class="{ 'opacity-0': isHovering }"
-    />
+    ></div>
 
-    <!-- Hover image -->
-    <img
-      :src="hoverSrc"
+    <!-- Hover image as background -->
+    <div
       ref="hoverImageRef"
-      alt="hover"
-      class="absolute inset-0 w-full h-full object-cover opacity-0"
+      class="absolute inset-0 w-full h-full object-cover opacity-0 bg-center bg-cover"
+      :style="{ backgroundImage: `url(${hoverSrc})` }"
       :class="{ 'opacity-100': isHovering }"
-    />
+    ></div>
 
     <!-- Flash overlay -->
     <div ref="flashRef" class="absolute inset-0 bg-white opacity-0 pointer-events-none"></div>
+
+    <!-- Transparent click-blocker -->
+    <div
+      class="absolute inset-0 z-10"
+      style="background: transparent"
+      @contextmenu.prevent
+      @dragstart.prevent
+    ></div>
   </div>
 </template>
 
